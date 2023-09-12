9b07e94946c31237932b22320fe3233f1038e7dd
Richard Günther <richardguenther@fastmail.com>
2023-09-12 02:46:22 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./content/**/*.{html,js,md,liquid,svg}", "./_includes/**/*.{html,js,md,liquid,svg}"],
+    theme: {
+      extend: {
+      },
+    },
+    plugins: [],
+  }
+
+  
